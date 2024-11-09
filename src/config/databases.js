109a97475f1ac1a7a5604ce5ab6ca88cbbf17a51
yashruhela821 <168224617+yashruhela821@users.noreply.github.com>
2024-11-09 20:36:25 +0530

    Update databases.js

--- src/config/databases.js
+++ src/config/databases.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-"mongodb+srv://yashruhela:cY4tODhJLZ9r5hQ6@cluster0.hlqfw.mongodb.net/Tindecompasss"
+""
   );
 };
 
